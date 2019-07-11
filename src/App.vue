719736c31f24fd8d7d9e2eecd59f5ec6d0da7e2c
qiushizhang <1031752581@qq.com>
2019-07-11 17:23:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="stylus" >
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './App'
+
+new Vue({ //  实例
+  el:'body', // 挂载点
+  components: {App}
+})
